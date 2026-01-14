960ec8805eb9599e077d1b1ceab58f6853e9fdc7
MauriceHPS <maurice@headpoint.info>
2026-01-15 00:52:28 +0300

    Changed the default font to google fonts. Error correction 2

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react";
-import Footer from "./Footer";
-import Header from "./Header";
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,11 +6,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <>
-      <div className="flex flex-col min-h-screen">
-        <h1>Welcome to Splash App</h1>
-      </div>
-    </>
+    <div>
+      <h1>Welcome to Splash App</h1>
+    </div>
   );
 };
 
